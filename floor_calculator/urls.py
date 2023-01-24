de2bef33c3ab3cc871aa7d72f3e97f5773eebca1
michaellafave <mlafave@waremalcomb.com>
2023-01-24 12:29:29 -0700

    updated sketch of ws calculator. will perform area calculations in views layer when we have instructions. flow ratio is governed by user requirements yet to be defined. need data to populate and test on.

--- floor_calculator/urls.py
+++ floor_calculator/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("floor_calculator", views.index) # if a request reaches floor_calc, exe function pointed to by views.index
-]
+    path("floor_calculator", views.index), # if a request reaches floor_calc, exe function pointed to by views.index
+    path("floor_calculator", views.post),
+    path("floor_calculator", views.update()),
+    path("floor_calculator", views.get()),
+    path("floor_calculator", views.delete())
+    ]
--- floor_calculator/views.py
+++ floor_calculator/views.py
@@ -5,3 +5,31 @@ from django.http import Http404
 
 def index(request):
     return HttpResponse("Working")
+
+def post(request):
+    if request != HttpResponseBadRequest:
+        #.write(request)
+        return HttpResponse("Request Posted")
+    else:
+        return("server side error")
+
+def delete(request, id,):
+        #if request != HttpResponseBadRequest and id != HttpResponseBadRequest:
+      #object.delete/flush(id)
+
+    #else
+    #return HttpResponseBadRequest
+    return HttpResponse("Record deleted")
+def update(request, id):
+    if request != HttpResponseBadRequest and id != HttpResponseBadRequest:
+    #HttpResponse.write(request, id)
+        return HttpResponse("Request Updated")
+    else:
+        return ("error 500")
+
+def get(request, id):
+    if request != HttpResponseBadRequest and id != HttpResponseBadRequest:
+        #return object.id
+        return HttpResponse("Here is your get request")
+    else:
+        return("server side error")
